f866b0cb4d6e743fe2562c8ce2dbaa3b46a735ba
satyanishika-31 <satyanishika-31@gmail.com>
2026-05-13 14:59:19 +0530

    Fix frontend import path for deployment

--- FRONT_blog/src/Components/Register.jsx
+++ FRONT_blog/src/Components/Register.jsx
@@ -9,7 +9,7 @@ import {
   pageBackground,
   submitBtn,
   mutedText,
-} from "../styles/common";
+} from "../Styles/Common";
 import { useForm } from "react-hook-form";
 import { NavLink, useNavigate } from "react-router";
 import { useState } from "react";
